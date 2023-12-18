67efc16cdb7da68ef2d0ca451a3be9bd072a046c
yoqubjanoff <yoqubjanoff@gmail.com>
2023-12-18 15:28:59 +0500

    ui continue

--- src/components/HomePagesComponents/MiddleSection/style.js
+++ src/components/HomePagesComponents/MiddleSection/style.js
@@ -10,7 +10,11 @@ MiddleStyle.Container = styled.div`
   max-width: 1210px;
   margin: 0 auto;
 `;
-MiddleStyle.Wrapper = styled.div``;
+MiddleStyle.Wrapper = styled.div`
+display: flex;
+align-items: center;
+gap: 42px;
+`;
 MiddleStyle.TextBox = styled.div``;
 MiddleStyle.MiddleTitle = styled.h3``;
 MiddleStyle.Desc = styled.p``;
@@ -18,7 +22,9 @@ MiddleStyle.ImageBox = styled.div`
 width: 100%;
 max-width: 404px;
 height: 581px;
-background-image: url(${middleBgImg})
+background-image: url(${middleBgImg});
+background-repeat: no-repeat;
+background-position: center;
 `;
 MiddleStyle.ImageSmallBox = styled.div``;
 MiddleStyle.InnerBox = styled.div``;
